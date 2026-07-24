58016a6a1376b63525ac13f50205bb62428ed18a
PranjalNishad <nishadpranjal450@gmail.com>
2026-07-24 16:53:17 +0530

    Add MongoDB connection and update dependencies
    - Implement MongoDB connection in mongo.config.ts
    - Update package.json and bun.lock with new dependencies: dotenv, mongoose, nanoid
    - Enhance app.ts to utilize nanoid for generating short URLs

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { nanoid } from "nanoid";
+import connectDB from "./config/mongo.config";
+import dotenv from "dotenv";
+dotenv.config()
 
-const app = new Hono()
-app.get('/', (c) => c.text('Hello Bun!'))
+const app = new Hono();
 
-export default {
-  port: 3000,
+connectDB();
+
+app.post("/api/create", async (c) => {
+  // Parse JSON body
+  const {url} = await c.req.json();
+
+  console.log(url);
+
+  return c.text(nanoid(7));
+});
+
+serve({
   fetch: app.fetch,
-}
+  port: 3000,
+});
+
+console.log("Server is running on http://localhost:3000");
+
+// get - redirection 
+// post - create short url 
